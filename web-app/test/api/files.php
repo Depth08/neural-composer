a786d4d26b8b30a6929f34523b09bff967813792
rafael.mindreau <rafael.mindreau@student.odisee.be>
2016-12-09 18:53:37 +0100

    API php file for files

--- web-app/test/api/files.php
+++ web-app/test/api/files.php
@@ -5,3 +5,17 @@
  * Date: 9/12/2016
  * Time: 14:02
  */
+error_reporting( E_ALL );
+
+if (isset($_POST['data']) && isset($_POST['name']) && strlen($_POST['name']) > 1) {
+    $text = json_encode($_POST['data']);
+    $filename = $_POST['name'];
+
+    file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR .  'trainingData' . DIRECTORY_SEPARATOR . $filename , $text);
+    echo 'Succesfully saved file';
+}
+else {
+    echo 'Could not save file';
+}
+
+?>
